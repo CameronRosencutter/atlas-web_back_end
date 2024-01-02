0448b9cf8d2deae9aa395b3fc523a3b9787f94b3
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-02 17:57:36 -0600

    ading

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Take str1 and str2 to and concencate as one.'''
+
+
+def concatenate_and_print(string1, string2):
+    result = string1 + string2
+    print(result)
+
+str1 = "Hello, "
+str2 = "world!"
+concatenate_and_print(str1, str2)
