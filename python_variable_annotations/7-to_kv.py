02533cbf956662513a619654b19a6ccd9668fea8
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-03 21:52:32 -0600

    fdsa

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+'''info'''
 
 
 from typing import Union, Tuple
---
+++ python_variable_annotations/8-make_multipliers.py
@@ -0,0 +1,9 @@
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """multiplies a float by the given multiplier."""
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
+
+    return multiplier_function
