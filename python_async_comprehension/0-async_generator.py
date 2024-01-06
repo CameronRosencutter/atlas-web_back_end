e31a86eac50496c6dd7f282a71c946b2788cd711
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-05 22:33:35 -0600

    dfasdfsa

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
+'''words'''
 
-"""jiljil"""
 import asyncio
 import random
 
-
 async def async_generator():
-    """10 random numbers"""
+    """morewords"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.randint(0, 10)
 
 
+# Example usage
 async def main():
-    # Create an event loop
-    loop = asyncio.get_event_loop()
+    """moremorewords"""
+    async for number in async_generator():
+        print(number)
 
-    # Run the async_generator and print each result
-    async for result in async_generator():
-        print(result)
 
-if __name__ == "__main__":
-    asyncio.run(main())
-    
+# Run the event loop
+asyncio.run(main())
