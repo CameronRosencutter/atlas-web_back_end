dee87f4a0e9d826647c5b6dc05430c5a720d462d
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-05 22:39:05 -0600

    areaew

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""will execute async_comprehension four times in parallel
-using asyncio.gather"""
+"""wfdsdsaf"""
 import asyncio
+import time
+
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """measure_runtime should measure the total runtime and return it."""
-    inicio = asyncio.get_event_loop().time()
-    await asyncio.gather(async_comprehension(),
-                         async_comprehension(),
-                         async_comprehension(),
-                         async_comprehension())
-    fin = asyncio.get_event_loop().time()
-    return fin - inicio
+    """fdsadfsa"""
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end = time.time()
+    return end - start
