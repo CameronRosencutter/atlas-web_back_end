1df59b48cd1e5a5dbf0c1eaf81e4a00d60154768
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-05 22:57:32 -0600

    fixes

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
-'''please please please work'''
 
+"""This will loop 10 times, then choose a number from 1-10 at random"""
 import asyncio
 import random
 
 
 async def async_generator():
-    """I dont know what is wrong anytmore"""
+    """loops several times, then chooses a number 1-10 at random"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
-
-
-# Example usage
-async def main():
-    """Am i eternally curesed? Why why why!!"""
-    async for number in async_generator():
-        print(number)
-
-
-# Run the event loop
-asyncio.run(main())
+        yield random.uniform(0, 10)
