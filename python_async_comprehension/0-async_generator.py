698edc054ea2f39b3e2aef897c66361117b2c483
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-05 22:25:49 -0600

    fereaw

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""write random number from 1 - 10"""
+
+"""task 0"""
+import asyncio, random
 
 
-import asyncio
-import random
-from typing import Generator
-
-
-async def async_generator() -> Generator[float, None, None]:
-    """Async generator that yields random numbers between 0 and 10"""
+async def async_generator():
+    """
+    Parameters: none
+    Returns: 10 random numbers
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
