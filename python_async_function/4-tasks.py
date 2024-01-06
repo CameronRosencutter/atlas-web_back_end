d50ced5cb7d7e5d8f29fd1dc7dc05dd7e12db8c7
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-05 18:09:23 -0600

    fdsadfsa

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""ffffa"""
+
+
+import asyncio
+from typing import List
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """dfsa"""
+    tasks = [await task_wait_random(max_delay) for _ in range(n)]
+    return sorted(tasks)
