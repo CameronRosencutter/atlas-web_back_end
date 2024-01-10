dc0a27da1f92d101cdad4c956f79afc43886c43b
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-09 20:10:21 -0600

    fdasdfsa

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
-""" first problem """
+"""
+    A basic caching algorithm.
+
+    Attributes:
+        capacity (int): Maximum number of items the cache can hold.
+        cache (dict): Dictionary to store key-value pairs.
+    """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Cache"""
+        """
+    A basic caching algorithm.
+
+    Attributes:
+        capacity (int): Maximum number of items the cache can hold.
+        cache (dict): Dictionary to store key-value pairs.
+    """
 
     def put(self, key, item):
         """put"""
