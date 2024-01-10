5e4810f53a0e9871d47cb878a0e3760d9161fc05
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-09 19:19:08 -0600

    fadsdfs

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,14 +1,13 @@
-from base_caching import BaseCaching
+#!/usr/bin/env python3
+
+""" first problem """
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """Cache"""
+
     def put(self, key, item):
+        """put"""
         if key is not None and item is not None:
             self.cache_data[key] = item
-
-    def get(self, key):
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        else:
-            return None
-        
