fe728f28138cce94c20a914f7daa6f78f15e1efa
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-10 15:27:28 -0600

    fdsafda

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+"""importing the base_caching file needed"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    def __init__(self):
+        """This is the init function needed"""
+        super().__init__()
+
+    def put(self, key, item):
+        """This is the put function needed in the code"""
+        if key is None or item is None:
+            return
+
+        # Check if the cache is full
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            # Get the most recently used key
+            mru_key = next(iter(self.cache_data))
+
+            # Discard the most recently used item
+            del self.cache_data[mru_key]
+            print(f"DISCARD: {mru_key}")
+
+        # Assign the item to the cache
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """This is the get function needed in the code"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        # Move the accessed key to the end to mark it as most recently used
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+
+        return value
