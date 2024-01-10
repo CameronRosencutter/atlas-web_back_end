7e2deb40f9fcfdde16a199239b4ce99eb6b2dc68
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-09 19:28:21 -0600

    adding all the mains

--- caching/base_caching.py
+++ caching/base_caching.py
@@ -2,14 +2,13 @@
 """ BaseCaching module
 """
 
-
 class BaseCaching():
     """ BaseCaching defines:
       - constants of your caching system
       - where your data are stored (in a dictionary)
     """
     MAX_ITEMS = 4
-
+    
     def __init__(self):
         """ Initiliaze
         """
@@ -25,9 +24,9 @@ class BaseCaching():
     def put(self, key, item):
         """ Add an item in the cache
         """
-        raise NotImplementedError("implemented in your cache class")
+        raise NotImplementedError("put must be implemented in your cache class")
 
     def get(self, key):
         """ Get an item by key
         """
-        raise NotImplementedError("implemented in your cache class")
+        raise NotImplementedError("get must be implemented in your cache class")
