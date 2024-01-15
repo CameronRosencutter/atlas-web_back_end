1239407325c202d248c58f9849f8d0edde7faf7a
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-15 17:01:55 -0600

    dadad

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -16,16 +16,15 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         log_message = super().format(record)
-        # print(log_message)
         result = filter_datum(self.fields, self.REDACTION,
                               log_message, self.SEPARATOR)
-        return(result)
+        return result
 
 
 def filter_datum(fields: List[str], redaction: str,
