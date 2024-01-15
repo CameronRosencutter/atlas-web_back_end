c0b346935272445182e2c889c065c87e401c1218
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-15 16:18:35 -0600

    jkj

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,4 +5,5 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    return re.sub(fr'(?<=\{separator}|^)({"|".join(fields)})\{separator}', f'{redaction}{separator}', message)
+    pattern = fr'({separator.join(fields)})=[^{separator}]+'
+    return re.sub(pattern, f'\\1={redaction}', message)
