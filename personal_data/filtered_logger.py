98005a46dea6fa4b425a91bed24136ffb5930842
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-15 17:42:09 -0600

    push

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -39,6 +39,7 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 def get_logger() -> logging.Logger:
+    """This will hopefully log everything correctly"""
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     logger.propagate = False
