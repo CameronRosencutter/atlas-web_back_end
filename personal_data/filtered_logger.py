0ea1875559ec08d680d672a25ac46723e198795f
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-16 15:37:05 -0600

    PUSH

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -51,3 +51,21 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connect to the MySQL database using environment variables."""
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = os.getenv('PERSONAL_DATA_DB_NAME', 'my_db')
+
+    # Create a connection to the MySQL database
+    db = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
+
+    return db
