378b7d15eec9fc18f51f148ab41cf8a0de9885cd
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-23 17:32:58 -0600

    fdsa

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,9 +2,12 @@
 """this is the auth.py file
 """
 
-from db import DB
-from user import User  # Assuming you have a User class defined
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+import uuid
 
 class Auth:
     """Auth class to interact with the authentication database.
