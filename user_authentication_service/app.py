db25856dff6fa6bd85d9bb9d73e824d638fde33f
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-23 17:22:57 -0600

    dsaf

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,16 +2,27 @@
 """this is the app.py file
 """
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
-
+AUTH = Auth()
 
 @app.route("/")
 def welcome():
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=["POST"])
+def register_user():
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        
+        AUTH.register_user(email, password)
+        
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
