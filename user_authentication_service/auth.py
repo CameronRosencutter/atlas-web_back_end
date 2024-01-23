2d95be1540ea44e52029bacdf488630c7f3fedcb
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-23 16:59:41 -0600

    fixes

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,13 +4,14 @@
 
 import bcrypt
 
+
 def _hash_password(password: str) -> bytes:
     """This will be the hash password part"""
     # Generate a random salt
     salt = bcrypt.gensalt()
-    
+
     # Hash the password with the generated salt
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    
+
     # Return the hashed password as bytes
     return hashed_password
