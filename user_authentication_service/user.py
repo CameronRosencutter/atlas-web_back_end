6d7089974eaaa4dd80c6b3a8367055d23c648339
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-23 14:58:21 -0600

    fixes

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -6,7 +6,9 @@ This is the uer.py file
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class User(Base):
     """This is the user base of the file"""
@@ -17,3 +19,4 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
     reset_token = Column(String, nullable=True)
+    
