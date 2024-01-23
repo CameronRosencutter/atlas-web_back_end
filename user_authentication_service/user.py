13c046c373f30cb47f6f27396056aaa0491e908a
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-23 14:57:04 -0600

    hjkhjk

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class User(Base):
+    """This is the user base of the file"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
