75423f0f678b93766be357b2a19f83130948e2e7
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-30 17:44:52 -0600

    pdss

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
+"""This is the 0app file in use"""
 
 # File: i18n/0-app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
