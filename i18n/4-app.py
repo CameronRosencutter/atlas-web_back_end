cbc6d1aab8efbeea87d7098b7a99e81f8cb239a8
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-01-31 21:10:30 -0600

    fdsa

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""4-app.py file"""
+"""4-app.py"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
 class Config:
-    """Configuration class"""
+    """Babel Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -16,21 +16,21 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale():
-    """function to get user locale"""
-    force_locale = request.args.get('locale')
-    if force_locale and force_locale in app.config['LANGUAGES']:
-        return force_locale
+    """This is the getlocale function"""
+    url_locale = request.args.get('locale')
+    if url_locale and url_locale in app.config['LANGUAGES']:
+        return url_locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """initial route to index"""
-    # from flask_babel import gettext as _
-    return render_template('4-index.html')
+    """GET method for index.html"""
+    from flask_babel import gettext as _
+    return render_template('/3-index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
