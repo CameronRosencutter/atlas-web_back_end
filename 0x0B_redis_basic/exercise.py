d488ce14306c11c61b1dce964e581233eeb14b41
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-02-11 19:10:20 -0600

    fdas

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,7 +5,7 @@ Main file
 
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 class Cache:
     def __init__(self):
@@ -16,3 +16,33 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=int)
+
+# Test cases
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
+
+print("All test cases passed successfully.")
+
