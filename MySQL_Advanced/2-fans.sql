9e4e8758daa0f35abf326193aae517e4c479ebdc
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-02-12 22:32:09 -0600

    dfsaadfs

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,7 +1,9 @@
 --2-fans.sql
 --ordered by fans all around.
 
-SELECT origin, SUM(fans) AS nb_fans
-FROM metal_bands
+SELECT origin AS origin, SUM(fans) AS nb_fans
+FROM  metal_bands
+WHERE origin IS NOT NULL AND fans IS NOT NULL
 GROUP BY origin
+ORDER BY nb_fans DESC;
 ORDER BY nb_fans DESC;
