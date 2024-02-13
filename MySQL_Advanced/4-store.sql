f99d00255a93c5a3e90b29b217fc854765998b27
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-02-12 22:55:11 -0600

    fdasfdsa

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,14 @@
+-- Create the trigger
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    -- Decrease the quantity of the item in the items table
+    UPDATE items
+    SET quantity = quantity - NEW.quantity_ordered
+    WHERE item_id = NEW.item_id;
+END//
+
+DELIMITER ;
