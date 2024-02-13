f8f05cf0e1092b07e5f72ce7c57c443c45f00939
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-02-12 20:14:22 -0600

    fdas

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -34,8 +34,8 @@ class Cache:
             return output
         return wrapper
 
-    @call_history
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
@@ -49,23 +49,4 @@ class Cache:
             return fn(data)
         return data
 
-    def get_str(self, key: str) -> Union[str, None]:
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
-
-    def get_int(self, key: str) -> Union[int, None]:
-        return self.get(key, fn=int)
-
-# Test cases
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
-
-print("All test cases passed successfully.")
+    def get_str(self, key: str) -> Unio
