12f8e234fc2bf5e4cb534bd67c3b8d6527749207
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-02-12 20:11:52 -0600

    pushhh

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -6,12 +6,23 @@ Main file
 import uuid
 import redis
 from typing import Union, Callable
+from functools import wraps
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @staticmethod
+    def count_calls(method: Callable) -> Callable:
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            key = method.__qualname__
+            self._redis.incr(key)
+            return method(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
@@ -45,4 +56,3 @@ for value, fn in TEST_CASES.items():
     assert cache.get(key, fn=fn) == value
 
 print("All test cases passed successfully.")
-
