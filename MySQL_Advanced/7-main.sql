8feba92846eb674cf923fe638c63079ed264315f
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-02-13 22:00:08 -0600

    fdsa

--- MySQL_Advanced/7-main.sql
+++
@@ -1,9 +0,0 @@
--- 7-main.sql
-SELECT * FROM users;
-SELECT * FROM corrections;
-
-SELECT "--";
-CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
-
-SELECT "--";
-SELECT * FROM users;
