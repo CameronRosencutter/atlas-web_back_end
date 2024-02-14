c63bcffbb31a0f427d063dc5195e0a7a74c9852b
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-02-13 21:58:25 -0600

    fsdsdaf

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,10 +1,12 @@
 --7-average_score.sql
+
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT)
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
-    SET avg_score = (SELECT AVG(score) FROM corrections AS correct WHERE correct.user_id=user_id);
-    UPDATE users SET average_score = avg_score WHERE id=user_id;
-END
-//
+    DECLARE average FLOAT;
+    SELECT AVG(score) INTO average FROM corrections WHERE corrections.user_id = user_id;
+    UPDATE users SET average_score = average WHERE id  = user_id;
+END //
+
+DELIMITER ;
