e29eb079882fe73fe8f73745f67e6c8ca97083c0
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-02-26 17:10:04 -0600

    fdas

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,5 +1,7 @@
-/** 
- * 0-console.js
+/** 0. Executing basic javascript with Node JS
+
+In the file 0-console.js, create a function named
+displayMessage that prints in STDOUT the string argument.
 */
 
 function displayMessage(message) {
