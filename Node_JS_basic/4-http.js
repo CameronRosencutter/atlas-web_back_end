ef9c8d2954cbe8993585890fadcde21b1ce7cdf1
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-02-27 20:18:01 -0600

    fdsa

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,16 +1,11 @@
+// Small HTTP server using the HTTP module
+
 const http = require('http');
 
-// Create HTTP server
 const app = http.createServer((req, res) => {
-    // Set response header
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    
-    // Send response body
-    res.end('Hello Holberton School!\n');
-});
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('Hello Holberton School!');
+  res.end();
+}).listen(1245);
 
-// Listen on port 1245
-app.listen(1245);
-
-// Export the app
 module.exports = app;
